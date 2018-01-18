f93b0c61012edb662cea3be322a47ce2fe57b586
Leslie <mouty216@gmail.com>
2018-01-18 21:49:33 +0800

    new struct exercise

---
+++ 039_struct.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import base64, struct
+
+bmp_data = base64.b64decode('Qk1oAgAAAAAAADYAAAAoAAAAHAAAAAoAAAABABAAAAAAADICAAASCwAAEgsAAAAAAAAAAAAA/3//f/9//3//f/9//3//f/9//3//f/9//3//f/9//3//f/9//3//f/9//3//f/9//3//f/9//3//f/9/AHwAfAB8AHwAfAB8AHwAfP9//3//fwB8AHwAfAB8/3//f/9/AHwAfAB8AHz/f/9//3//f/9//38AfAB8AHwAfAB8AHwAfAB8AHz/f/9//38AfAB8/3//f/9//3//fwB8AHz/f/9//3//f/9//3//f/9/AHwAfP9//3//f/9/AHwAfP9//3//fwB8AHz/f/9//3//f/9/AHwAfP9//3//f/9//3//f/9//38AfAB8AHwAfAB8AHwAfP9//3//f/9/AHwAfP9//3//f/9//38AfAB8/3//f/9//3//f/9//3//fwB8AHwAfAB8AHwAfAB8/3//f/9//38AfAB8/3//f/9//3//fwB8AHz/f/9//3//f/9//3//f/9/AHwAfP9//3//f/9/AHwAfP9//3//fwB8AHz/f/9/AHz/f/9/AHwAfP9//38AfP9//3//f/9/AHwAfAB8AHwAfAB8AHwAfAB8/3//f/9/AHwAfP9//38AfAB8AHwAfAB8AHwAfAB8/3//f/9//38AfAB8AHwAfAB8AHwAfAB8/3//f/9/AHwAfAB8AHz/fwB8AHwAfAB8AHwAfAB8AHz/f/9//3//f/9//3//f/9//3//f/9//3//f/9//3//f/9//3//f/9//3//f/9//3//f/9//3//f/9//38AAA==')
+
+
+def bmp_info(data):
+	if len(data) < 30:
+		print('This is not a bitmap!')
+	else:
+		info = struct.unpack('<ccIIIIIIHH', data[:30])
+		if info[0] == b'B' and (info[1] == b'M' or info[2] == b'A'):
+			return {
+				'width': info[6],
+				'height': info[7],
+				'color': info[9]
+			}
+# 测试
+bi = bmp_info(bmp_data)
+assert bi['width'] == 28
+assert bi['height'] == 10
+assert bi['color'] == 16
+print('ok')
+print(bi)
